e8702a8e69f78b087ca433f65fe2353e01d71243
Leandro Bernardo <lbsantos1@stefanini.com>
2022-07-11 11:54:15 -0300

    first component | notification button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
+import NotificationButton from './components/NotificationButton/index';
+
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <h1>Hello World!</h1>
+      <NotificationButton />
+    </>
   )
 }
 
